482ebf7946afffa8905b0807df4a358e0a5d19fe
waqas <waqaskazmi2006@gmail.com>
2023-04-17 17:50:10 +0500

    my first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const winston = require('winston');
+const express = require('express');
+const app = express();
+
+require('./startup/logging')();
+require('./startup/routes')(app);
+require('./startup/db')();
+require('./startup/config')();
+require('./startup/validation')();
+require('./startup/prod')(app);
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => winston.info(`listening on port ${port} ....`));
+
---
+++ startup/db.js
@@ -0,0 +1,6 @@
+const winston = require('winston');
+const mongoose= require('mongoose');
+module.exports = function (){
+    mongoose.connect('mongodb://127.0.0.1/vidly')
+.then(() => winston.info("connected to mongodb database vidly") );
+}
