730f4b6b3858cc9987e1b52808d9e53fde0ef3cb
abc <abc.com>
2023-04-18 18:12:13 +0500

    cloud db

--- startup/db.js
+++ startup/db.js
@@ -1,6 +1,8 @@
 const winston = require('winston');
 const mongoose= require('mongoose');
 module.exports = function (){
-    mongoose.connect('mongodb://127.0.0.1/vidly')
+    //mongoose.connect('mongodb://127.0.0.1/vidly')
+    mongoose.connect('mongodb+srv://waqaskazmi2006:$ensation5@cluster0.kgbdso7.mongodb.net/?retryWrites=true&w=majority')
+    
 .then(() => winston.info("connected to mongodb database vidly") );
 }
