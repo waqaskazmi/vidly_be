2ec515bd81c57f0699c46c3e4acf4211301fa6d1
abc <abc.com>
2023-04-21 22:47:44 +0500

    user api status code bug fixed

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,7 @@ router.post("/", async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({email : req.body.email});
-  if (user) return res.status(500).send("User already registered");
+  if (user) return res.status(404).send("User already registered");
 
   user = new User(_.pick(req.body, ['name' , 'email', 'password']));
   const salt = await bcrypt.genSalt(10);
